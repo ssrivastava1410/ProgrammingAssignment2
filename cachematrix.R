5b02c96d825466e71dee4a8cb5d00d0bfbf3127c
Srivastava <saur.srivastava@cgi.com>
2020-08-03 22:56:15 +0200

    commit the changes for missing bracket

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##function makeCacheMatrix:
+##1. set the value of the matrix
+##2. get the value of the matrix
+##3. set the value of the inverse
+##4. get the value of the inverse
+## 
+##: cacheSolve:
+##Computes the inverse of the special "matrix" returned 
+##by makeCacheMatrix aboveT. If the inverse has already been calculated 
+##(and the matrix has not changed), then the cachesolve should retrieve the 
+##inverse from the cache.
 
-## Write a short comment describing this function
+## setting and getting the special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invrs <- NULL
+        set <- function(y) {
+                x <<- y
+                invrs <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) invrs <<- solve
+        getsolve <- function() invrs
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+##by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invrs <- x$getsolve()
+        if(!is.null(invrs)) {
+                message("getting cached data for inverse matrix")
+                return(invrs)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(invrs)
+        invrs           
 }
